32e83be541b8bffb1cd8465dec94d3142aa411c4
Dennis Vo <dennisvo95@gmail.com>
2026-09-01 06:03:19 -0700

    update user, authcontroller, authroute

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const express = require('express');
 const dns = require("dns");
 const connectDB = require('./config/db');
 const app = express();
-
+const userRouter = require("../routes/authRoutes.js")
 require('dotenv').config();
 
 app.use(express.json());
 
-dns.setServers(["1.1.1.1","8.8.8.8"])
+dns.setServers(["1.1.1.1", "8.8.8.8"])
 
-
+app.use("/api/user", userRouter)
 const PORT = process.env.PORT || 5000;
 
 // Connect to MongoDB Database
